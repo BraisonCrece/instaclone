51f5038eda43a0c7dc56abba4b37816101e4c8a6
Brais Fernandez <braisoncrece@gmail.com>
2022-09-06 14:10:21 +0200

    turbo frame issues solved

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+  
   def create
     if params[:likeable_type] == 'Post'
       @likeable =  Post.find(params[:post_id])
@@ -10,11 +11,11 @@ class LikesController < ApplicationController
 
   def destroy
     if params[:likeable_type] == 'Post'
-      @likeable =  current_user.posts.find_by(id: params[:post_id])
-      current_user.likes.where(likeable_type: 'Post', likeable_id: @likeable.id ).delete_all
+      @likeable =  Post.find(params[:post_id])
+      Like.where(likeable_type: 'Post', likeable_id: @likeable.id ).delete_all
     elsif params[:likeable_type] == 'Comment'
-      @likeable = current_user.comments.find_by(id: params[:comment_id])
-      current_user.likes.where(likeable_type: 'Comment', likeable_id: @likeable.id).delete_all
+      @likeable = Comment.find(params[:comment_id])
+      Like.where(likeable_type: 'Comment', likeable_id: @likeable.id).delete_all
     end    
   end
 
