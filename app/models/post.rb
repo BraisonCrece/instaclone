fe546633b28f7c36289cf33bd657943443bd7fb7
Brais Fernandez <braisoncrece@gmail.com>
2022-09-15 10:24:56 +0200

    attached image to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
+  has_one_attached :image
   belongs_to :user, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
+
+  validates :image, :body, presence: true
 end
